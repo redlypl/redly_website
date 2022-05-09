309d21f9d2588ef10490b01249d92b29a8fb30c7
Bart <bartek@redly.pl>
2022-05-09 18:21:35 +0200

    404 page - grid template was completed

---
+++ src/templates/404/404.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+import {
+    HeroWrapper,
+    ContentSide,
+    SculptureSide,
+    
+} from "../../styles/templates/404/404.style"
+
+const NotFound = () => {
+    return (
+        <HeroWrapper>
+            <ContentSide>
+                ContentSide
+            </ContentSide>
+            <SculptureSide>
+                SculptureSide
+            </SculptureSide>
+        </HeroWrapper>
+    )
+}
+
+export default NotFound
