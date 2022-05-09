6c775b5a80d18362121f84915af90cd173c01cb0
Bart <bartek@redly.pl>
2022-05-09 18:59:08 +0200

    contact page was added

---
+++ src/pages/kontakt.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout/layout"
+
+import Contact from "../templates/universal/contact"
+
+const KontaktPage = () => {
+    return (
+        <Layout>
+            <Contact isIndex= { true } />
+        </Layout>
+    )
+}
+
+export default KontaktPage
