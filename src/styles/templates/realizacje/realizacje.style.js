1dea5b75d3d12dbba044457f0bacf0fc22533d4b
Bart <bartek@redly.pl>
2022-06-11 08:35:05 +0200

    realizacje.js - items list has been added

--- src/styles/templates/realizacje/realizacje.style.js
+++ src/styles/templates/realizacje/realizacje.style.js
@@ -1,6 +1,11 @@
 import styled from "styled-components"
 import { theme } from "../../theme"
 
+import clipPathPng from "../../../images/realizations-clip-path.png"
+import clipPathPngMobile from "../../../images/realizations-clip-path-mobile.png"
+
+import cursorPointer from "../../../images/cursor-pointer-finger-emoji.png"
+
 export const RealizacjeWrapper = styled.div`
     padding: 50px 200px;
     display: flex;
@@ -15,10 +20,10 @@ export const RealizacjeWrapper = styled.div`
         padding: 25px 50px;
     }
     @media (max-width: ${theme.breakpoint.tablet}) {
-        padding: 25px 30px;
+        padding: 25px 0;
     }
     @media (max-width: ${theme.breakpoint.mobile}) {
-        padding: 25px 10px;
+        padding: 25px 0;
     }
 `
 export const HeaderWrapper = styled.div`
@@ -31,11 +36,10 @@ export const HeaderWrapper = styled.div`
     }
 `
 export const RealizationsItemsWrapper = styled.div`
-    background-color: darkcyan;
     display: flex;
     flex-direction: column;
     justify-content: space-around;
-    gap: 5px;
+    gap: 15px;
     padding: 0 150px;
 
     @media (max-width: ${theme.breakpoint.largeDesktop}) {
@@ -46,9 +50,21 @@ export const RealizationsItemsWrapper = styled.div`
     }
 `
 export const RealizationItem = styled.div`
-    background-color: honeydew;
     height: 300px;
     width: 100%;
+    box-shadow: 0px 3px 17px #00000019;
+    background-color: #F5F5F5;
+    border-radius: 17px;
+    display: flex;
+    cursor: url(${cursorPointer}), auto;
+
+    :hover > .hoverBg {
+        opacity: 1;
+    }
+
+    :hover {
+        transform: scale(1.015);
+    }
 
     @media (max-width: ${theme.breakpoint.laptop}) {
         height: 270px;
@@ -58,5 +74,117 @@ export const RealizationItem = styled.div`
     }
     @media (max-width: ${theme.breakpoint.mobile}) {
         height: 150px;
+    }
+`
+export const ContentItemWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    height: 100%;
+    width: 50%;
+
+    @media (max-width: ${theme.breakpoint.tablet}) {
+        width: 60%;
+    }
+`
+export const DecoLine = styled.div`
+    background-color: black;
+    height: 4px;
+    width: 50px;
+
+    @media (max-width: ${theme.breakpoint.desktop}) {
+        width: 30px;
+    }
+    @media (max-width: ${theme.breakpoint.tablet}) {
+        width: 10px;
+    }
+`
+export const Number = styled.p`
+    font-size: 40px;
+    font-weight: 700;
+    padding: 0 40px;
+
+    @media (max-width: ${theme.breakpoint.largeDesktop}) {
+        padding: 0 35px;
+    }
+    @media (max-width: ${theme.breakpoint.desktop}) {
+        padding: 0 20px;
+    }
+    @media (max-width: ${theme.breakpoint.laptop}) {
+        padding: 0 15px;
+        font-size: 35px;
+    }
+    @media (max-width: ${theme.breakpoint.tablet}) {
+        padding: 0 15px;
+        font-size: 25px;
+    }
+`
+export const HeaderWrapperItemItem = styled.div`
+
+`
+export const TitleParagraph = styled.h6`
+    font-size: 53px;
+    line-height: 63px;
+    margin: 0;
+
+    @media (max-width: ${theme.breakpoint.largeDesktop}) {
+        font-size: 47px;
+        line-height: 57px;
+    }
+    @media (max-width: ${theme.breakpoint.laptop}) {
+        font-size: 35px;
+        line-height: 45px;
+    }
+    @media (max-width: ${theme.breakpoint.tablet}) {
+        font-size: 30px;
+        line-height: 40px;
+    }
+    @media (max-width: ${theme.breakpoint.mobile}) {
+        font-size: 20px;
+        line-height: 30px;
+    }
+`
+export const CategoryParagraph = styled.p`
+    margin: 0;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 17px;
+
+    @media (max-width: ${theme.breakpoint.tablet}) {
+        font-size: 14px;
+        line-height: 24px;
+    }
+    @media (max-width: ${theme.breakpoint.mobile}) {
+        font-size: 12px;
+        line-height: 22px;
+    }
+`
+export const DateParagraph = styled.p`
+    margin: 0;
+    text-transform: uppercase;
+    font-weight: 500;
+    font-size: 13px;
+
+    @media (max-width: ${theme.breakpoint.tablet}) {
+        font-size: 12px;
+        line-height: 22px;
+    }
+    @media (max-width: ${theme.breakpoint.mobile}) {
+        font-size: 11px;
+        line-height: 21px;
+    }
+`
+export const ThumbnailItemWrapper = styled.div`
+    mask-image: url( ${clipPathPng} );
+    mask-size: 100% 100%;
+    height: 100%;
+    width: 50%;
+    background-image: ${props => `url(${props.background})`};
+    background-size: cover;
+    background-position: center;
+    opacity: 0.7;
+
+    @media (max-width: ${theme.breakpoint.tablet}) {
+        mask-image: url( ${clipPathPngMobile} );
+        width: 40%;
     }
 `
--- src/templates/realizacje/realizacje.js
+++ src/templates/realizacje/realizacje.js
@@ -5,11 +5,68 @@ import {
     HeaderWrapper,
     RealizationsItemsWrapper,
     RealizationItem,
+    ContentItemWrapper,
+    DecoLine,
+    Number,
+    HeaderWrapperItemItem,
+    TitleParagraph,
+    CategoryParagraph,
+    DateParagraph,
+    ThumbnailItemWrapper,
 
 } from "../../styles/templates/realizacje/realizacje.style"
 
 import Header from "../../components/header/header"
 import { EmojiData } from "../../components/apple-emojis/emojiData"
+
+import wroconsult from "../../images/portfolio/wroconsult.jpg"
+import redly from "../../images/portfolio/redly.jpg"
+import unknown from "../../images/portfolio/unknown.jpg"
+
+const RealizationData = [
+    {
+        id: '06',
+        name: 'Wroconsult',
+        category: 'Strona internetowa',
+        date: '23 sierpień 2022',
+        thumbnail: wroconsult
+    },
+    {
+        id: '05',
+        name: 'Redly',
+        category: 'Kampania reklamowa',
+        date: '23 sierpień 2022',
+        thumbnail: redly
+    },
+    {
+        id: '04',
+        name: 'Unknow art',
+        category: 'Branding',
+        date: '23 sierpień 2022',
+        thumbnail: unknown
+    },
+    {
+        id: '03',
+        name: 'Redly',
+        category: 'Kampania reklamowa',
+        date: '23 sierpień 2022',
+        thumbnail: redly
+    },
+    {
+        id: '02',
+        name: 'Wroconsult',
+        category: 'Strona internetowa',
+        date: '23 sierpień 2022',
+        thumbnail: wroconsult
+    },
+    {
+        id: '01',
+        name: 'Unknow art',
+        category: 'Branding',
+        date: '23 sierpień 2022',
+        thumbnail: unknown
+    }
+]
 
 const Realizacje = () => {
     return (
@@ -21,15 +78,25 @@ const Realizacje = () => {
                 />
             </HeaderWrapper>
             <RealizationsItemsWrapper>
-                <RealizationItem>first-item</RealizationItem>
-                <RealizationItem>second-item</RealizationItem>
-                <RealizationItem>third-item</RealizationItem>
-                <RealizationItem>first-item</RealizationItem>
-                <RealizationItem>second-item</RealizationItem>
-                <RealizationItem>third-item</RealizationItem>
-                <RealizationItem>first-item</RealizationItem>
-                <RealizationItem>second-item</RealizationItem>
-                <RealizationItem>third-item</RealizationItem>
+                {RealizationData.map((item) => {
+                    return (
+                        <RealizationItem key={item.id}>
+                            <ContentItemWrapper>
+                                <DecoLine />
+                                <Number>{item.id}</Number>
+                                <HeaderWrapperItemItem>
+                                <TitleParagraph>{item.name}</TitleParagraph>
+                                <CategoryParagraph>{item.category}</CategoryParagraph>
+                                <DateParagraph>{item.date}</DateParagraph>
+                                </HeaderWrapperItemItem>
+                            </ContentItemWrapper>
+                            <ThumbnailItemWrapper
+                                className="hoverBg"
+                                background={item.thumbnail}
+                            />
+                        </RealizationItem>
+                    )
+                })}
             </RealizationsItemsWrapper>
         </RealizacjeWrapper>
     )
