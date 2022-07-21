e08f8fd226026ce3a5db3c5c8fcc629b18853dfd
Bart <bartek@redly.pl>
2022-07-21 13:21:37 +0200

    problem with icons and service worker was solved

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,10 @@ module.exports = {
       },
     resolve: 'gatsby-source-contentful',
     options: {
-      "accessToken": `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
-      "spaceId": `${process.env.CONTENTFUL_SPACE_ID}`
+      // "accessToken": `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+      // "spaceId": `${process.env.CONTENTFUL_SPACE_ID}`
+      "accessToken": `UmEaUAQ-sbH8ZONRvbgFSf8rdGT1kq6wDDU6guB4CsQ`,
+      "spaceId": `jqub8nvgwkb4`
     }
   }, "gatsby-plugin-styled-components", {
     resolve: 'gatsby-plugin-google-analytics',
@@ -31,15 +33,67 @@ module.exports = {
   }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png",
+      "icon": `./src/images/favicons/icon.png`,
+      "icons": [
+        {
+          src: `./src/images/favicons/icon-48x48.png`,
+          sizes: `48x48`,
+          type: `image/png`,
+          purpose: "any"
+        },
+        {
+          src: `./src/images/favicons/icon-72x72.png`,
+          sizes: `72x72`,
+          type: `image/png`,
+          purpose: "any"
+        },
+        {
+          src: `./src/images/favicons/icon-96x96.png`,
+          sizes: `96x96`,
+          type: `image/png`,
+          purpose: "any"
+        },
+        {
+          src: `./src/images/favicons/icon-144x144.png`,
+          sizes: `144x144`,
+          type: `image/png`,
+          purpose: "any"
+        },
+        {
+          src: `./src/images/favicons/icon-192x192.png`,
+          sizes: `192x192`,
+          type: `image/png`,
+          purpose: "any"
+        },
+        {
+          src: `./src/images/favicons/icon-256x256.png`,
+          sizes: `256x256`,
+          type: `image/png`,
+          purpose: "any"
+        },
+        {
+          src: `./src/images/favicons/icon-384x384.png`,
+          sizes: `384x384`,
+          type: `image/png`,
+          purpose: "any"
+        },
+        {
+          src: `./src/images/favicons/icon-512x512.png`,
+          sizes: `512x512`,
+          type: `image/png`,
+          purpose: "any"
+        },
+      ],
       "name": "Redly - Agencja Reklamowa",
       "short_name": `Agencja Reklamowa z nowoczesnym podejściem do marketingu`,
       "start_url": `/`,
+      "scope": ".",
       //"background_color": `#f7f0eb`,
       //"theme_color": `#a2466c`,
       "display": `standalone`,
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  }, 'gatsby-plugin-offline',
+  "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
