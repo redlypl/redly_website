05b525738cf50bcc1174f3b051fe438b83ee23fc
Bart <bartek@redly.pl>
2022-07-22 11:57:02 +0200

    56d-vgh

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,10 +22,8 @@ module.exports = {
       },
     resolve: 'gatsby-source-contentful',
     options: {
-      // "accessToken": `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
-      // "spaceId": `${process.env.CONTENTFUL_SPACE_ID}`
-      "accessToken": `UmEaUAQ-sbH8ZONRvbgFSf8rdGT1kq6wDDU6guB4CsQ`,
-      "spaceId": `jqub8nvgwkb4`
+      "accessToken": `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+      "spaceId": `${process.env.CONTENTFUL_SPACE_ID}`
     }
   }, "gatsby-plugin-styled-components", {
     resolve: 'gatsby-plugin-google-analytics',
