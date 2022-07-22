8bb4b3a361c73afa0cfc2aab1c1739de93b9f7ab
Bart <bartek@redly.pl>
2022-07-22 16:22:32 +0200

    change gtag head option to true

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: false,
+          head: true,
         },
       },
     },
